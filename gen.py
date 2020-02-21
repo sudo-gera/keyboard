cd6192e62b8dd2d3fe5e51d5528775f72622010e
sudo-gera <gera.tatarinov.999@gmail.com>
2020-02-21 21:13:06 +0300

    j

--- gen.py
+++ gen.py
@@ -1,5 +1,7 @@
 def create(b,e):
- ext=open('hide_'+str(b)+'_'+str(e)+'.xml','w')
+ if (e-b)==2**10:
+  print(e,end='\r')
+ ext=open('hide_auto_'+str(b)+'_'+str(e)+'.xml','w')
  ext.write('<?xml version="1.0" encoding="utf-8"?>\n<Keyboard\nxmlns:android="http://schemas.android.com/apk/res/android"\nandroid:keyWidth="'+('6.25' if (b-e)<=256 else '12.5')+'%p">\n'
  if e-b<=256:
   for q in range(b,e,16):
