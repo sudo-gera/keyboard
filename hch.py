e21a1ed565474d3b80e97944cab3e69b9c6fabdd
sudo-gera <gera.tatarinov.999@gmail.com>
2021-02-17 21:05:01 +0300

    receiveing...

--- hch.py
+++
@@ -1,8 +0,0 @@
-from os import listdir
-for w in listdir('.'):
- try:
-  q=open(w).read()
-  q=q.replace('<Row android:keyHeight="66%p">\n  </Row>\n </Keyboard>','<Row android:keyHeight="0%p">\n  </Row>\n </Keyboard>')
-  open(w,'w').write(q)
- except:
-  pass
